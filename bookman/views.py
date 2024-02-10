27e34f66b4f4135c4ba59f85d687b688d3c2a588
yoshi <yoshitakaOkada0214@gmail.com>
2024-02-10 11:22:25 +0900

    paginationを削除

--- bookman/views.py
+++ bookman/views.py
@@ -1,5 +1,4 @@
-from django.db.models import Q
-from rest_framework import generics, response, pagination
+from rest_framework import generics
 from .models import Book, Category
 from .serializers import CategorySerializer, BookListSerializer, BookDetailSerializer
 
@@ -9,42 +8,9 @@ class CategoryList(generics.ListAPIView):
     serializer_class = CategorySerializer
 
 
-class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 3
-
-    def get_paginated_response(self, data):
-        return response.Response({
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'count': self.page.paginator.count,
-            'total_pages': self.page.paginator.num_pages,
-            'current_page': self.page.number,
-            'results': data,
-            'page_size': self.page_size,
-            'range_first': (self.page.number * self.page_size) - self.page_size + 1,
-            'range_last': min((self.page.number * self.page_size), self.page.paginator.count),
-        })
-
-
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all().order_by('category')
     serializer_class = BookListSerializer
-    pagination_class = StandardResultsSetPagination
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        keyword = self.request.query_params.get('keyword', None)
-        if keyword:
-            queryset = queryset.filter(
-                Q(name__icontains=keyword) |
-                Q(author__name__icontains=keyword) |
-                Q(lead_text__icontains=keyword)
-            )
-        category = self.request.query_params.get('category', None)
-        if category:
-            queryset = queryset.filter(category=category)
-
-        return queryset
 
 
 class BookDetail(generics.RetrieveAPIView):
