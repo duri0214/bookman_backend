6e505c9f91702d65a98ad5c8da86e4a3a4a495eb
yoshi <yoshitakaOkada0214@gmail.com>
2026-07-19 14:02:58 +0900

    #3 Add black formatter

--- bookman/models.py
+++ bookman/models.py
@@ -6,6 +6,7 @@ class Branch(models.Model):
     """
     図書館支店マスタ
     """
+
     name = models.CharField(max_length=255, unique=True)
     address = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
@@ -14,25 +15,25 @@ class Branch(models.Model):
     updated_at = models.DateField(auto_now=True, null=True)
 
     class Meta:
-        db_table = 'bookman_m_branch'
+        db_table = "bookman_m_branch"
 
     def __str__(self):
         return self.name
 
 
 class Category(models.Model):
-    name = models.CharField('カテゴリ名', max_length=100, unique=True)
-    color = models.CharField('色(16進数)', max_length=7, default='#000000')
+    name = models.CharField("カテゴリ名", max_length=100, unique=True)
+    color = models.CharField("色(16進数)", max_length=7, default="#000000")
 
     class Meta:
-        db_table = 'bookman_m_category'
+        db_table = "bookman_m_category"
 
     def __str__(self):
         return self.name
 
 
 class Author(models.Model):
-    name = models.CharField('著者名', max_length=255, unique=True)
+    name = models.CharField("著者名", max_length=255, unique=True)
 
     def __str__(self):
         return self.name
@@ -43,16 +44,19 @@ class Book(models.Model):
     書籍マスタ
     システムを使用するひとつの自治体が束ねる、n個の支店図書館すべてが所蔵する本
     """
-    name = models.CharField('タイトル', max_length=255, unique=True)
-    thumbnail = models.ImageField('サムネイル', blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='カテゴリ')
-    authors = models.ManyToManyField(Author, verbose_name='著者')
-    lead_text = models.TextField('紹介文')
-    amount = models.PositiveSmallIntegerField('数量')
-    isbn = models.CharField('ISBNコード', max_length=20)
-    publication_date = models.DateField('出版年月日')
-    created_at = models.DateField('登録日', auto_now_add=True)
-    updated_at = models.DateField('更新日', auto_now=True, null=True)
+
+    name = models.CharField("タイトル", max_length=255, unique=True)
+    thumbnail = models.ImageField("サムネイル", blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, verbose_name="カテゴリ"
+    )
+    authors = models.ManyToManyField(Author, verbose_name="著者")
+    lead_text = models.TextField("紹介文")
+    amount = models.PositiveSmallIntegerField("数量")
+    isbn = models.CharField("ISBNコード", max_length=20)
+    publication_date = models.DateField("出版年月日")
+    created_at = models.DateField("登録日", auto_now_add=True)
+    updated_at = models.DateField("更新日", auto_now=True, null=True)
 
     def __str__(self):
         return self.name
@@ -63,8 +67,9 @@ class Assignment(models.Model):
     システムを使用するひとつの自治体が束ねる、n個の支店図書館がそれぞれどの本をいくつ所蔵するか
     ある支店図書館にある本の数量合計が、Bookテーブルの amount と一致する
     """
-    branch = models.ForeignKey('Branch', on_delete=models.CASCADE)
-    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+
+    branch = models.ForeignKey("Branch", on_delete=models.CASCADE)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
     amount = models.PositiveSmallIntegerField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True, null=True)
@@ -78,10 +83,15 @@ class Lending(models.Model):
     貸出日と created_at は同じになる
     返却が終わると active が 0 になる
     """
+
     return_date = models.DateField()
-    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
     active = models.BooleanField(default=1)
-    customer_user = models.ForeignKey(User, related_name='customer', on_delete=models.CASCADE)
-    contact_user = models.ForeignKey(User, related_name='contact', on_delete=models.CASCADE)
+    customer_user = models.ForeignKey(
+        User, related_name="customer", on_delete=models.CASCADE
+    )
+    contact_user = models.ForeignKey(
+        User, related_name="contact", on_delete=models.CASCADE
+    )
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True, null=True)
--- bookman/serializers.py
+++ bookman/serializers.py
@@ -5,34 +5,37 @@ from bookman.models import Branch, Book, Category, Author
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'color']
+        fields = ["id", "name", "color"]
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Branch
-        fields = ['id', 'name', 'address', 'phone', 'remark']
+        fields = ["id", "name", "address", "phone", "remark"]
 
 
 class BookSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-    authors = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), many=True)
+    authors = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(), many=True
+    )
 
     class Meta:
         model = Book
-        fields = ['id',
-                  'name',
-                  'category',
-                  'thumbnail',
-                  'authors',
-                  'lead_text',
-                  'amount',
-                  'isbn',
-                  'publication_date'
-                  ]
+        fields = [
+            "id",
+            "name",
+            "category",
+            "thumbnail",
+            "authors",
+            "lead_text",
+            "amount",
+            "isbn",
+            "publication_date",
+        ]
