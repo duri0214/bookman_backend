f2eb6c0b6205715b9f5fe17f3202b4462810b57c
yoshi <yoshitakaOkada0214@gmail.com>
2026-07-19 15:38:18 +0900

    #4 Allow frontend IP origin

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://153.126.200.229:3000",
+]
 
 ROOT_URLCONF = "config.urls"
 
